1668d98f27222f24c80aa0f7cb0f7c8f3ce7841d
NhuNgoc <nguyennhungoc810@gmail.com>
2022-12-13 14:01:35 +0700

    add setting app

--- FE/app/App.js
+++ FE/app/App.js
@@ -1,20 +1,114 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  CardStyleInterpolators,
+  createStackNavigator,
+} from "@react-navigation/stack";
+import AnimatedLottieView from "lottie-react-native";
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from "react-native";
+import { Provider } from "react-native-paper";
+import AnimTab from "./src/components/BottomTab/AnimTab";
+import Colors from "./src/constants/Colors";
+import DetailScreen from "./src/screens/ListScreens/DetailScreen";
+import LoginScreen from "./src/screens/LoginScreen";
+import RegisterScreen from "./src/screens/RegisterScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const isDarkMode = useColorScheme() === "dark";
+  const [loaded, setLoaded] = useState(false);
+
+  const backgroundStyle = {
+    flex: 1,
+    backgroundColor: isDarkMode ? Colors.black : Colors.white,
+  };
+
+  if (loaded == false)
+    return (
+      <View style={styles.splash}>
+        <Text style={styles.textWelcome}>Hi</Text>
+        <AnimatedLottieView
+          source={require("./assets/splash.json")}
+          autoPlay
+          duration={3000}
+          loop={false}
+          onAnimationFinish={() => {
+            setLoaded(true);
+          }}
+        />
+      </View>
+    );
+  else
+    return (
+      <Provider>
+        <SafeAreaView style={backgroundStyle}>
+          <StatusBar
+            barStyle={isDarkMode ? "light-content" : "dark-content"}
+            backgroundColor={Colors.white}
+          />
+          <NavigationContainer>
+            <RootStack />
+          </NavigationContainer>
+        </SafeAreaView>
+      </Provider>
+    );
 }
+
+const screenOptions = {
+  gestureEnabled: true,
+  gestureDirection: "horizontal",
+  cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+  headerShown: false,
+};
+
+const Stack = createStackNavigator();
+
+const RootStack = () => {
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen
+        name="Tab"
+        component={AnimTab}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Register"
+        component={RegisterScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="DetailScreen"
+        component={DetailScreen}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  textWelcome: {
+    flex: 1,
+    fontSize: 50,
+    textAlign: "center",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  splash: { flex: 1, alighItems: "center" },
 });
